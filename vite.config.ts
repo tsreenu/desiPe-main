d39b91c814aa58e50cb140e705ddfc17005b68cf
Sreenu Thuraka <tsreenu.9@gmail.com>
2025-06-09 12:38:07 +0530

    feat: add homepage and deploy script to package.json; remove staticwebapp.config.json; update vite.config.ts for GitHub Pages; add CNAME for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import path from "path";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/", // ✅ important for GitHub Pages!
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
